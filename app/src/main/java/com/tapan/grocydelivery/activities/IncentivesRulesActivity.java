263ffd1cc72b9dda8dde9bb7b7e7be069996081e
tapanyadav <yadav.tapanpr@gmail.com>
2020-08-19 11:55:05 +0530

    IncentivesRulesActivity is added...

---
+++ app/src/main/java/com/tapan/grocydelivery/activities/IncentivesRulesActivity.java
@@ -0,0 +1,18 @@
+package com.tapan.grocydelivery.activities;
+
+import android.os.Bundle;
+
+import com.tapan.grocydelivery.R;
+
+public class IncentivesRulesActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_incentives_rules;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
