ca9bc34be0d53ffd26b65da54db7deab8d8a879e
tapanyadav <yadav.tapanpr@gmail.com>
2020-08-19 11:29:12 +0530

    OpenSourceLibActivity is added...

---
+++ app/src/main/java/com/tapan/grocydelivery/activities/OpenSourceLibActivity.java
@@ -0,0 +1,18 @@
+package com.tapan.grocydelivery.activities;
+
+import android.os.Bundle;
+
+import com.tapan.grocydelivery.R;
+
+public class OpenSourceLibActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_open_source_lib;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
